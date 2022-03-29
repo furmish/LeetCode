05fc6e24e1db64ec8db26937958a11518ab25717
Trilliant <mrzeero0@mail.ru>
2022-03-29 22:08:57 +0300

    initial commit

---
+++ Arrays/src/MergeSortedArray/Solution.java
@@ -0,0 +1,21 @@
+package MergeSortedArray;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {1,2,3,0,0,0};
+        int[] nums2 = new int[] {2,5,6};
+        int m = 3, n = 3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
+//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+//The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+            System.arraycopy(nums2, 0, nums1, m, n);
+            Arrays.sort(nums1);
+    }
+}
