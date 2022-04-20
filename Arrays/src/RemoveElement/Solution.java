c42e4176b0631d5cd96012e582450e2dfdc223bf
Trilliant <mrzeero0@mail.ru>
2022-04-20 18:36:44 +0300

    initial commit

---
+++ Arrays/src/RemoveElement/Solution.java
@@ -0,0 +1,18 @@
+package RemoveElement;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[] {0,1,2,2,3,0,4,2}, 2));
+
+    }
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val)
+                nums[k++] = nums[i];
+        }
+        return k;
+    }
+}
