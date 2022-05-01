3efc2a825c46b6869a447972952f458fbe58efdf
Trilliant <mrzeero0@mail.ru>
2022-05-01 22:54:48 +0300

    ref

--- Arrays/src/RemoveElement/Solution.java
+++ Arrays/src/RemoveElement/Solution.java
@@ -1,11 +1,8 @@
 package RemoveElement;
 
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
         System.out.println(removeElement(new int[] {0,1,2,2,3,0,4,2}, 2));
-
     }
     public static int removeElement(int[] nums, int val) {
         int k = 0;
