9b6c7d78f46e20d088b26abd6e43c5d9e74a7bcd
Trilliant <mrzeero0@mail.ru>
2022-05-06 11:41:58 +0300

    ref

--- Arrays/src/SquaresOfSortedArray/Solution.java
+++ Arrays/src/SquaresOfSortedArray/Solution.java
@@ -4,14 +4,23 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sortedSquares(new int[]{-7,-3,2,3,11})));
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-7, -3, 2, 3, 11})));
     }
+
     public static int[] sortedSquares(int[] nums) {
-        int length = nums.length;
-        int[] result = new int[length];
-        for (int i = 0; i < length; i++)
-            result[i] = nums[i] * nums[i];
-        Arrays.sort(result);
-        return result;
+        int l = 0;
+        int r = nums.length - 1;
+        int[] res = new int[nums.length];
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (Math.abs(nums[l]) > Math.abs(nums[r])) {
+                res[i] = nums[l] * nums[l];
+                l++;
+            } else {
+                res[i] = nums[r] * nums[r];
+                r--;
+            }
+        }
+        return res;
     }
 }
