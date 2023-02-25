5418d9ae9805766a463c2e570ddd7987b47cc25e
furmish <furmish@vk.com>
2023-02-25 22:41:46 +0300

    initial

---
+++ Problems/BestTimeToBuyAndSellStock/Solution.java
@@ -0,0 +1,26 @@
+package BestTimeToBuyAndSellStock;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxSell = 0;
+        int maxProfit = 0;
+
+        for (int i = prices.length - 1; i >= 0; i--) {
+            if (prices[i] > maxSell) {
+                maxSell = prices[i];
+            }
+            else {
+                maxProfit = Math.max(maxSell - prices[i], maxProfit);
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxProfit(new int[] {7,1,5,3,6,4}));
+        System.out.println(solution.maxProfit(new int[] {7,6,4,3,1}));
+        System.out.println(solution.maxProfit(new int[] {2,5,1}));
+    }
+}
