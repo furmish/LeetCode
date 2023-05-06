54fda03c5045b75f5f11654d6db39c3ef86945dd
furmish <furmish@vk.com>
2023-05-06 14:56:57 +0300

    refactored

--- Problems/missing_positive/Solution.java
+++ Problems/missing_positive/Solution.java
@@ -35,8 +35,8 @@ public class Solution {
 
     private int findKthPositive(int[] arr, int k) {
         int [] missingInts = new int[2001];
-        for (int i = 0; i < arr.length; i++) {
-            missingInts[arr[i]] = arr[i];
+        for (int value : arr) {
+            missingInts[value] = value;
         }
         int counter = 0;
         for (int j = 1; j < 2001; j++) {
